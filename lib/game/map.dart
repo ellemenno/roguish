356ba077df46272cbac81fcd13819515ab452d09
ellemenno <ellemenno@users.noreply.github.com>
2022-03-05 08:19:42 -0500

    add rectangle and test room

--- lib/game/map.dart
+++ lib/game/map.dart
@@ -1,6 +1,9 @@
+import 'dart:math' as math;
 
 import 'package:rougish/game/game_data.dart';
 
+//import 'src/creature.dart';
+//import 'src/item.dart';
 import 'src/map_types.dart';
 import 'src/map_symbols.dart';
 
@@ -61,6 +64,72 @@ class Cell {
   Cell(this.col, this.row);
 }
 
+class Rectangle {
+  int left;
+  int top;
+  int right;
+  int bottom;
+
+  int get width => right - left;
+  int get height => bottom - top;
+
+  @override
+  String toString() { return '[${left},${top},${right},${bottom}]'; }
+
+  Rectangle(this.left, this.top, this.right, this.bottom);
+
+  Rectangle.square(int size) :
+    left = 0,
+    top = 0,
+    right = size,
+    bottom = size;
+
+  Rectangle.byDimension(int width, int height) :
+    left = 0,
+    top = 0,
+    right = width,
+    bottom = height;
+
+  static Rectangle bounding(Rectangle r1, Rectangle r2) {
+    return Rectangle(
+      math.min(r1.left, r2.left),
+      math.min(r1.top, r2.top),
+      math.max(r1.right, r2.right),
+      math.max(r1.bottom, r2.bottom)
+    );
+  }
+
+  static Rectangle splitH(Rectangle r1, int distance) {
+    if (distance >= r1.width) { throw Exception('rectangle of width ${r1.width} can not be split at ${distance}'); }
+    Rectangle r2 = Rectangle(r1.right - distance, r1.top, r1.right, r1.bottom);
+    r1.right -= distance;
+    return r2;
+  }
+
+  static Rectangle splitV(Rectangle r1, int distance) {
+    if (distance >= r1.height) { throw Exception('rectangle of height ${r1.height} can not be split at ${distance}'); }
+    Rectangle r2 = Rectangle(r1.left, r1.top + distance, r1.right, r1.bottom);
+    r1.bottom -= distance;
+    return r2;
+  }
+
+  static void resize(Rectangle r, { int dl = 0, int dt = 0, int dr = 0, int db = 0 }) {
+    r.left += dl;
+    r.top += dt;
+    r.right += dr;
+    r.bottom += db;
+    if (r.width <= 3) { throw Exception('rectangle resized to invalid width'); }
+    if (r.height <= 3) { throw Exception('rectangle resized to invalid height'); }
+  }
+
+  static void translate(Rectangle r, { int dx = 0, int dy = 0 }) {
+    r.left += dx;
+    r.top += dy;
+    r.right += dx;
+    r.bottom += dy;
+  }
+}
+
 class LevelGenerator {
 
   static void _fill(List<List<Cell>> map, int cols, int rows) {
@@ -74,11 +143,28 @@ class LevelGenerator {
     }
   }
 
+  static void _room(List<List<Cell>> map, Rectangle coords) {
+    int c1 = coords.left;
+    int c2 = coords.right;
+    int r1 = coords.top;
+    int r2 = coords.bottom;
+    for (int c = c1; c <= c2; c++) {
+      map[r1][c].type = CellType.wallDim;
+      map[r2][c].type = CellType.wallDim;
+    }
+    for (int r = r1+1; r < r2; r++) {
+      map[r][c1].type = CellType.wallDim;
+      map[r][c2].type = CellType.wallDim;
+    }
+  }
+
   static void generate(GameData gameData, {cols = 80, rows = 24}) {
     List<List<Cell>> map = gameData.levelMap;
     _fill(map, cols, rows);
 
     // eventually, smart stuff to populate the cells..
+    Rectangle coords = Rectangle(2,4,8,12);
+    _room(map, coords);
     map.first.first.type = CellType.tunnelDim;
     map.first.last.type = CellType.tunnelBright;
     map.last.first.type = CellType.wallBright;
